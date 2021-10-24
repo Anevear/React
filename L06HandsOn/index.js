3d531ca68b8543870e153f480dc6718b2bbb4bc4
Anevear <anevear2180@gmail.com>
2021-10-23 23:40:43 -0500

    l6Ho

--- L06HandsOn/index.js
+++ L06HandsOn/index.js
@@ -1,21 +1,15 @@
-// Requirements :: Add the code for the following requirements within the index.js file.
-
 // Step 1 Convert the given JavaScript into ES6 syntax using an arrow function and filter.
+var filtered=car =>{
+    return car.name === 'ford';
+}
 
 const arr = [
    {"name":"chevy", "count": 2},
    {"name":"ford", "count": 5},
    {"name":"acura", "count": 3},
    {"name":"honda", "count": 16},
-];
+].filter(filtered);
 
-const newArr = [];
-
-for(let i= 0; i < arr.length; i++){
-    if(arr[i].name === "ford" ){
-       newArr.push(arr[i]);
-   }
-}
 console.log("Filter results:", newArr);
 
 // Step 2 Given the function, use the ES6 that was covered to combine the two arrays.
@@ -23,11 +17,6 @@ console.log("Filter results:", newArr);
 const arr = ['Bill', 'Joe', 'Emily', 'Andrea']
 const newStudents = ['Andrew', 'Tasha', 'Carol', 'George'];
 
-function addNewStudent(array, newArray) {
-    for(let i = 0; i < newArray.length; i++) {
-        array.push(newArray[i]);
-    }
-}
-addNewStudent(arr, newStudents);
+const mixArray = [...arr, ...newStudents];
 
-// Tip! ::: You will need to use the spread operator!
+console.log(mixArray);
